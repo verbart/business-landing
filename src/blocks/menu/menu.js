1c6269a88773635fa2241be945d7bd5853e40e47
Artem Verbat <artem.verbat@gmail.com>
2020-01-27 22:40:59 +0200

    A little code reformat.

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,13 +1,12 @@
 import $ from 'jquery';
 
-
 $('.menu').on('click', '.menu__link', function (e) {
     e.preventDefault();
 
     const anchor = $($(this).attr('href'));
     const top = anchor.offset().top - $('.navbar').height() + 1;
 
-    $('body,html').animate({scrollTop: top}, 1000);
+    $('body,html').animate({ scrollTop: top }, 1000);
     $('.navbar__menu.is-open').removeClass('is-open');
 });
 
@@ -19,7 +18,7 @@ $(window).scroll(function () {
         const bottom = top + section.height();
         const scroll = $(window).scrollTop();
 
-        if( scroll > top && scroll < bottom){
+        if (scroll > top && scroll < bottom) {
             $('.menu__item_active').removeClass('menu__item_active');
             menuLink.closest('.menu__item').addClass('menu__item_active');
         }
